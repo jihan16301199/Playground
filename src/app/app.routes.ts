f4352b9413453a5ec38d68cbcdf727e4b6edae2c
Jihan Alam <jihan@ridecentric.com>
2026-02-05 04:07:25 +0600

    Refactor code structure for improved readability and maintainability

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,13 @@
 import { Routes } from '@angular/router';
 import { Layout } from './features/layout/layout';
+import { Login } from './authentication/login/login';
 
 export const routes: Routes = [
+
+    {
+        path: 'login',
+        component: Login
+    },
     {
         path: '',
         component: Layout
