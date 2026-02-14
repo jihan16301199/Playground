c64a46ce784c0871ef6177e8dbfd0b1cc63ff68b
Jihan Alam <jihan@ridecentric.com>
2026-02-15 04:05:04 +0600

    login

--- src/app/authentication/login/login.ts
+++ src/app/authentication/login/login.ts
@@ -1,13 +1,58 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.html',
-  styleUrl: './login.scss'
+  styleUrl: './login.scss',
+  animations: [
+    trigger('slideDown', [
+      transition(':enter', [
+        style({
+          transform: 'translateY(-100%)',
+          opacity: 0
+        }),
+        animate('150ms ease-out', style({
+          transform: 'translateY(0)',
+          opacity: 1
+        }))
+      ]),
+      transition(':leave', [
+        animate('150ms ease-in', style({
+          transform: 'translateY(-100%)',
+          opacity: 0
+        }))
+      ])
+    ])
+  ]
 })
 export class Login {
+
+  loginForm;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.nonNullable.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  get f() {
+    return this.loginForm.controls;
+  }
+
+  onSubmit() {
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.loginForm.value);
+  }
+
   current: number = 0;
 
   toStep(index: number) {
